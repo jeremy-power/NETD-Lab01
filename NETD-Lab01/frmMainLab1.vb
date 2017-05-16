571d9a41ba5a6dafd3387c2b0119375edb648438
subzero9998 <reaper9998@hotmail.com>
2017-05-16 10:53:27 -0400

    commenting complete

--- NETD-Lab01/frmMainLab1.vb
+++ NETD-Lab01/frmMainLab1.vb
@@ -29,75 +29,97 @@
 
     End Sub
 
+    ''' <summary>
+    ''' Handles clicking the clear button
+    ''' Clears all input and output fields
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
 
+        'clears drink input fields
         txtDrinks.Clear()
         lbxDrinkType.ClearSelected()
+
+        'sets all output labels to "$0.00"
         lblDrinkVal.Text = "$0.00"
         lblMealVal.Text = "$0.00"
         lblSnackVal.Text = "$0.00"
         lblTotalVal.Text = "$0.00"
 
+        'clears all meal radio buttons
         For Each radBut As RadioButton In grpMeals.Controls
             radBut.Checked = False
         Next
 
+        'clears all snack checkboxes
         For Each cbx As CheckBox In grpSnacks.Controls
             cbx.Checked = False
         Next
 
     End Sub
 
+    ''' <summary>
+    ''' Handles the Calculate button click, 
+    ''' checks for input validation, if input
+    ''' passes it calls calcOrder
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
         'drink text box validation
-        If IsNumeric(txtDrinks.Text.Trim) Then
-            Dim drinksDecimal As Decimal
-            Dim drinksInt As Integer
-            If Decimal.TryParse(txtDrinks.Text.Trim, drinksDecimal) And Int32.TryParse(txtDrinks.Text.Trim, drinksInt) Then
 
-                If drinksDecimal = Math.Floor(drinksDecimal) Then
-                    'if valid input, creates variable representing the drink number
+        'variable to hold valid drink number
+        Dim drinksInt As Integer
 
-                    If drinksInt > 0 And drinksInt < 6 Then
-                        'drink type validation
-                        If lbxDrinkType.SelectedIndex = -1 Then
-                            MessageBox.Show("Must select a drink type")
+        'checks if input is valid
+        If Int32.TryParse(txtDrinks.Text.Trim, drinksInt) Then
 
-                            'meal selection validation
-                        Else
-                            Dim mealSelected As Boolean = False
-                            For Each radbut As RadioButton In grpMeals.Controls
-                                If radbut.Checked Then
-                                    mealSelected = True
-                                End If
-                            Next
-                            If Not mealSelected Then
-                                MessageBox.Show("Must select a meal")
+            'checks if valid drink number between 1 and 5
+            If drinksInt > 0 And drinksInt < 6 Then
 
-                                'if all input passes validation, proceeds with calculation
-                            Else
-                                Call calcOrder(drinksInt)
-                            End If
-                        End If
-                    Else
-                        MessageBox.Show("Must a be a whole number between 1 and 5")
-                        txtDrinks.Focus()
-                    End If
+                'checks if drink box empty
+                If lbxDrinkType.SelectedIndex = -1 Then
+                    MessageBox.Show("Must select a drink type")
+
 
                 Else
-                    MessageBox.Show("Must a be a whole number between 1 and 5")
-                    txtDrinks.Focus()
+                    'checks each meal radio button to ensure one is checked
+                    Dim mealSelected As Boolean = False
+                    For Each radbut As RadioButton In grpMeals.Controls
+                        If radbut.Checked Then
+                            mealSelected = True
+                        End If
+                    Next
+                    'prints error if no radio button is checked
+                    If Not mealSelected Then
+                        MessageBox.Show("Must select a meal")
+
+                        'if all input passes validation, proceeds with calculation
+                    Else
+                        Call calcOrder(drinksInt)
+                    End If
                 End If
+
+                'Error handle for drink number not between 1 and 5
             Else
                 MessageBox.Show("Must a be a whole number between 1 and 5")
                 txtDrinks.Focus()
             End If
+            'Error handle for invalid drink number (decimal or string)
         Else
-            MessageBox.Show("Must be a whole number between 1 and 5")
+            MessageBox.Show("Must a be a whole number between 1 and 5", "Error")
             txtDrinks.Focus()
-        End If
+            End If
     End Sub
 
+    ''' <summary>
+    ''' Takes in the integer number of drinks
+    ''' checked by input validation and calculates
+    ''' the $ total of each food/drink and sets
+    ''' label text values to the output
+    ''' </summary>
+    ''' <param name="drinksInt"></param>
     Private Sub calcOrder(ByVal drinksInt As Integer)
         Dim drinkTotal As Integer = 0
         Dim mealTotal As Integer = 0
@@ -151,10 +173,21 @@
 
     End Sub
 
+    ''' <summary>
+    ''' Handler for music check button, 
+    ''' plays music when checked on, 
+    ''' stops it when checked off
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub btnMusic_CheckedChanged(sender As Object, e As EventArgs) Handles btnMusic.CheckedChanged
+
+        'checks if it is checking or unchecking
         If btnMusic.Checked = True Then
+            'plays music
             My.Computer.Audio.Play(My.Resources.Cantina, AudioPlayMode.BackgroundLoop)
         Else
+            'stops music
             My.Computer.Audio.Stop()
         End If
 
